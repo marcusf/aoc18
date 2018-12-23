3126f9b4a9ae86aa0a05e1d4eebab06545b87e0d
marcusf <marcusf@spotify.com>
2018-12-23 14:51:56 +0100

    08 igen

--- 08.py
+++ 08.py
@@ -3,12 +3,12 @@ from anytree import Node, PreOrderIter
 d = list(map(int,open('08.in').read().split(' ')))
 
 def deserialize(d, t, parent):
-  children, entries, start, lis = d[t], d[t+1], t+2, list()
-  node = Node(lis, parent=parent)
+  children, entries, start, lst = d[t], d[t+1], t+2, list()
+  node = Node(lst, parent=parent)
   for i in range(0, children):
     if len(d) - start <= 2: break
     start, node = deserialize(d, start, node)
-  lis.extend(d[start:(start+entries)])
+  lst.extend(d[start:(start+entries)])
   return (start+entries, parent)
 
 def value(node):
