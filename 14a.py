b3a23b382f819db33b5cabeb803e72517a9f701a
marcusf <marcusf@spotify.com>
2018-12-25 23:03:48 +0100

    fixed 14

--- 14a.py
+++ 14a.py
@@ -30,25 +30,9 @@ def part1():
   while len(lst) < MAX+10:
     n = lst[e1]+lst[e2]
     new = digitize(n)
-    #new = [int(l) for l in list(str(n))]
-    #print(new)
     lst.extend(new)
     e1, e2 = (e1+1+lst[e1]) % len(lst), (e2+1+lst[e2]) % len(lst)
   
   print(''.join([str(s) for s in lst[MAX:(MAX+10)]])) 
 
-def part2():
-  e1,e2,lst=0,1,[3,7]
-  i = 0
-  VAL = [int(l) for l in list('509671')]
-  while not lst[-len(VAL):] == VAL:
-    i = i + 1
-    if i % 1000000 == 0: print("{:,}".format(i), "{:,}".format(len(lst)))
-    n = lst[e1]+lst[e2]
-    new = digitize(n)
-    lst.extend(new)
-    e1, e2 = (e1+1+lst[e1]) % len(lst), (e2+1+lst[e2]) % len(lst)
-  print(len(lst[0:-len(VAL)]))
-
-#part1()
-part2() # 583 607 528 # 5 982 162
+part1()
